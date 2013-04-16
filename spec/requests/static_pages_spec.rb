1a35ecd748e17360ae20b87920543a2fbb4ea0bf
Tom Joseph <thomas.w.joseph@dartmouth.edu>
2013-04-15 21:27:14 -0400

    Add application helper full_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "StaticPages" do
 	
   describe "Home Page" do
   	
-    it "should have the contect 'Sample App' " do
+    it "should have the content 'Sample App' " do
     	visit '/static_pages/home'
     	page.should have_selector('h1',
     														:text => "Sample App") 
@@ -15,14 +15,21 @@ describe "StaticPages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-      					:text => "#{base_title} | Home")
+      					:text => "#{base_title}")
     end 
+    
+    it "should not have custom page title" do
+    	visit '/static_pages/home'
+    	page.should_not have_selector('title',
+    						:text => ' | Home')
+  	end   
+    
   end # home page do
   
   
   describe "Help Page" do
   	
-  	it "should have contect 'Help'" do
+  	it "should have content 'Help'" do
   		visit '/static_pages/help'
     	page.should have_selector('h1',
     														:text => "Help") 
@@ -36,7 +43,7 @@ describe "StaticPages" do
 	end # help page do
 
   describe "About Page" do
-  	it "should have contect 'About Us'" do
+  	it "should have content 'About Us'" do
   		visit '/static_pages/about'
     	page.should have_selector('h1',
     														:text => "About Us") 
@@ -51,7 +58,7 @@ describe "StaticPages" do
 	end # about page do	
 	
   describe "Contact Page" do
-  	it "should have contect 'Contact Us'" do
+  	it "should have content 'Contact Us'" do
   		visit '/static_pages/contact'
     	page.should have_selector('h1',
     														:text => "Contact Us") 
